db900124f2b5d70dd86768d18ec54b476d2bb407
MithuBitz <mithubortamuly@gmail.com>
2020-09-29 00:20:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/weatherforcast/data/ApixuWeatherApiService.kt
@@ -0,0 +1,9 @@
+package com.example.weatherforcast.data
+
+
+const val API_KEY = "86a85c7a3a9ba17ecb356d449da0be17"
+
+//http://api.weatherstack.com/current?access_key=86a85c7a3a9ba17ecb356d449da0be17&query=New%20York
+
+interface ApixuWeatherApiService {
+}
---
+++ app/src/main/java/com/example/weatherforcast/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weatherforcast.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
